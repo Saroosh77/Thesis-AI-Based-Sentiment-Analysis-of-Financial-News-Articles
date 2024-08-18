42775e853dee857003274644610beff7640226eb
ma16266 <muhammad.asif@stud.th-deg.de>
2024-08-18 06:27:17 +0200

    upgate google scraper

--- Sentiment-Analysis/google_scraper.py
+++ Sentiment-Analysis/google_scraper.py
@@ -68,32 +68,6 @@ def save_in_google_news(query, article_list):
     conn.close()
 
 
-def save_in_google(query, article_list):
-    # Connect to MySQL database
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-
-    # Create a table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS google_articles (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            company_name VARCHAR(255),
-            article_link TEXT
-        )
-    ''')
-
-    # Insert data into the table
-    for link in article_list:
-        cursor.execute('''
-            INSERT IGNORE INTO google_articles (company_name, article_link)
-            VALUES (%s, %s)
-        ''', (query, link))
-
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
-
-
 if __name__ == "__main__":
     # search_query = input("Enter the search query: ")
     with open('companies.json', 'r') as file:
